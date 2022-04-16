67f3b9742a79092faffcceeb0f6642fb27b8ab8f
nicola <nicola.lamattina@outlook.com>
2022-04-16 22:16:51 +0200

    MUTED

--- cogs/ambience.py
+++ cogs/ambience.py
@@ -24,12 +24,30 @@ class Ambiance(commands.Cog):
         await ctx.send("JAUNE !!!!!!!!!!!!!!!!!") 
         await ctx.send("https://tenor.com/view/yu-gi-oh-gx-ojama-anime-monster-gif-17847003") 
 
+    @commands.command()
+    async def emotional(self, ctx):
+        await ctx.send("https://tenor.com/view/emotional-damage-emotional-damage-meme-funny-gif-24332819")
         
     @commands.command()
     async def ojamaSucks(self,ctx):
         await ctx.author.edit(nick="ojama slave" )
 
-    
+    @commands.command()
+    async def mute(self, ctx):
+        await ctx.send(f'MUTED {ctx.author.mention} !')
+        await ctx.author.edit(mute=True)
+        
+    @commands.command()
+    async def unmute(self, ctx):
+        await ctx.send("DEMUTED !")
+        await ctx.author.edit(mute=True)
+        
+    @commands.command()
+    async def TgJustin(self, ctx):
+        author = ctx.author.name
+        if(author == 'Kioyaa#7859'):
+            await ctx.send(ctx.author)
+        
         
 def setup(bot):
     bot.add_cog(Ambiance(bot))
