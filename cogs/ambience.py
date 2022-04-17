8a05468909f3ad3d4117ba9d8e0d493861441fd1
Appez <37080870+Rs-appez@users.noreply.github.com>
2022-04-18 01:28:06 +0200

    ugrade ambiance

--- cogs/ambience.py
+++ cogs/ambience.py
@@ -17,7 +17,7 @@ class Ambiance(commands.Cog):
     async def mp (self,ctx):
         member : Member =  ctx.author
         dm_chan = await member.create_dm()
-        await dm_chan.send("hello")
+        await dm_chan.send("https://static.wikia.nocookie.net/yugioh-gx/images/3/38/Ojama_Delta_Combin%C3%A9.png/revision/latest?cb=20140311110956&path-prefix=fr")
 
     @commands.command()
     async def ojama(self,ctx):
@@ -29,18 +29,13 @@ class Ambiance(commands.Cog):
         await ctx.send("https://tenor.com/view/emotional-damage-emotional-damage-meme-funny-gif-24332819")
         
     @commands.command()
+    async def baobaboon(self, ctx):
+        await ctx.send("https://www.google.com/url?sa=i&url=https%3A%2F%2Fdb.ygoprodeck.com%2Fcard%2F%3Fsearch%3DBaobaboon&psig=AOvVaw38GKreKmNu415uta-7DAJQ&ust=1650321878244000&source=images&cd=vfe&ved=0CAwQjRxqFwoTCPDO56GWnPcCFQAAAAAdAAAAABAR")
+        
+    @commands.command()
     async def ojamaSucks(self,ctx):
         await ctx.author.edit(nick="ojama slave" )
 
-    @commands.command()
-    async def mute(self, ctx):
-        await ctx.send(f'MUTED {ctx.author.mention} !')
-        await ctx.author.edit(mute=True)
-        
-    @commands.command()
-    async def unmute(self, ctx):
-        await ctx.send("DEMUTED !")
-        await ctx.author.edit(mute=False)
 
     @commands.command()
     async def tgjustin(self, ctx : commands.Context):
