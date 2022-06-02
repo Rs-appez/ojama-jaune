6d5bf2503d69f7942b3447b5334fc3df0245df1f
Appez <37080870+Rs-appez@users.noreply.github.com>
2022-06-02 21:31:10 +0000

    fix vocal

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import bots.ojamaBot as ojamaBot
 import config
-
+from serv import keep_alive
 #------------BOT PROD--------------------------
 
 ojama_jaune = ojamaBot.OjamaBot("!")
@@ -10,4 +10,5 @@ for file in os.listdir("./cogs"):
     if(file.endswith(".py")):
         ojama_jaune.load_extension(f"cogs.{file[:-3]}")
 
+keep_alive()
 ojama_jaune.run(config.OJAMA_JAUNE_TOKEN)
