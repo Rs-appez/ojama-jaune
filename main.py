2aa10c7f1abe0026c3f0e57e71ad730ca0b90b23
Appez <37080870+Rs-appez@users.noreply.github.com>
2022-12-20 16:24:10 +0100

    revome old/useless file

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
 import bots.ojamaBot as ojamaBot
 import config
-from serv import keep_alive
 #------------BOT PROD--------------------------
 
 ojama_jaune = ojamaBot.OjamaBot("!")
@@ -10,5 +9,4 @@ for file in os.listdir("./cogs"):
     if(file.endswith(".py")):
         ojama_jaune.load_extension(f"cogs.{file[:-3]}")
 
-keep_alive()
 ojama_jaune.run(config.OJAMA_JAUNE_TOKEN)
