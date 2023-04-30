704754d3ef6459ee1225a2a7bc631e7c383cc70c
Appez <37080870+Rs-appez@users.noreply.github.com>
2023-04-30 21:24:03 +0200

    add button et check letter

--- cogs/game.py
+++ cogs/game.py
@@ -4,22 +4,19 @@ from nextcord import slash_command
 
 from models.card.cards import Cards
 from models.game.hangman import Hangman
+from views.hangman.letter_view import Letter_view
 
 
 class Game(commands.Cog):
     """some games"""
     def __init__(self,bot):
         self.bot = bot
-
     @slash_command(name="pendu_yugioh",description="Crois en l'âme des cartes!")
     async def hangman(self,interaction : Interaction ):
         # card = Cards.get_random_card()
-        hangman = Hangman()
-        game_msg = await interaction.send(f"```{hangman.dispaly()}```")
-        for i in range(10):
-            hangman.add_error()
-            await game_msg.edit(f"```{hangman.dispaly()}```")
-      
-        
+        hangman = Hangman("test")
+        hangman.game_msg = await interaction.send(f"```{hangman.dispaly()}```")
+        await interaction.channel.send("Voyelle :",view=Letter_view(hangman,True))
+        await interaction.channel.send("Consonne :",view=Letter_view(hangman,False))
 def setup(bot):
     bot.add_cog(Game(bot))
--- models/game/hangman.py
+++ models/game/hangman.py
@@ -1,5 +1,5 @@
 class Hangman():
-    _hangman = [
+    __hangman = [
                    "            \n\n\n\n\n\n   ",
                    "            \n\n\n\n\n\n/-\\   ",
                    "            \n\n\n\n\n |\n/-\\   ",
@@ -11,9 +11,23 @@ class Hangman():
                    "            \n _______\n |     |\n |     O\n |\n |\n/-\\   ",
                    "            \n _______\n |     |\n |     O\n |    /|\\\n |\n/-\\   ",
                    "            \n _______\n |     |\n |     O\n |    /|\\\n |    / \\\n/-\\   "]
-    error = 0
+    __error = 0
+    game_msg = None
+
+    def __init__(self, word : str):
+        self.word = word.lower()
+        
 
     def dispaly(self):
-        return self._hangman[self.error]
-    def add_error(self):
-        self.error+=1 
+        return self.__hangman[self.__error]
+    
+    async def add_error(self):
+        self.__error+=1 
+        await self.game_msg.edit(f"```{self.dispaly()}```")
+
+    async def check_letter(self, letter : str):
+        if letter.lower() in self.word :
+            return True
+        else :
+            await self.add_error()
+            return False
