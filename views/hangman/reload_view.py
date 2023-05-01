218629bfc5e340a9a3dadbb6c18142a52765e187
Appez <37080870+Rs-appez@users.noreply.github.com>
2023-05-01 13:03:28 +0200

    fix double reload

--- views/hangman/reload_view.py
+++ views/hangman/reload_view.py
@@ -6,10 +6,13 @@ from nextcord.ui import View, button
 class ReloadView(View):
     def __init__(self,gm) -> None:
         self.gm = gm
+        self.click = False
         super().__init__()
 
     @button(label="AGAIN !",style=ButtonStyle.primary,emoji="🔁")
     async def reload(self,button,interaction):
-        button.disabled = True
-        await interaction.response.edit_message(view=self)
-        await self.gm.reload(interaction)
+        if not self.click:
+            self.click = True
+            button.disabled = True
+            await interaction.response.edit_message(view=self)
+            await self.gm.reload(interaction)
