c7e211653b839dba0595c8e958858096643e6d55
Appez <37080870+Rs-appez@users.noreply.github.com>
2023-05-14 16:52:35 +0200

    add response msg

--- cogs/game.py
+++ cogs/game.py
@@ -17,20 +17,23 @@ class Game(commands.Cog):
         gm = GameManager()
         game_channel = await self.create_game_channel(interaction,"pendu")
         await gm.hangman_yugioh(game_channel)
+        await interaction.response.send_message(f"GAME ! {self.bot.oj_emoji}")
+
 
     @slash_command(name="guess_the_card",description="Crois en l'âme des cartes!")
     async def guess(self,interaction : Interaction ):
         gm = GameManager()
         game_channel = await self.create_game_channel(interaction,"guess the card")
         await gm.guess_the_card(game_channel,self.bot.game_emojis)
+        await interaction.response.send_message(f"GAME ! {self.bot.oj_emoji}")
         
     async def create_game_channel(self,interaction : Interaction,name_channel):
 
         if interaction.channel.type == ChannelType.private :
             game_channel = interaction.channel
         else :
-            game_channel = await interaction.channel.create_thread(name=name_channel,reason = f"{name_channel} started",auto_archive_duration=60)
-            await game_channel.add_user(interaction.user)
+            game_channel = await interaction.channel.create_thread(name=name_channel,reason = f"{name_channel} started",auto_archive_duration=60,type=ChannelType.public_thread)
+            game_channel.auto_archive_duration = 1
         return game_channel
 
 def setup(bot):
