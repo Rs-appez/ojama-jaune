74d30cd33d908a1cf2a25c08b290857df2b85903
Appez <37080870+Rs-appez@users.noreply.github.com>
2023-08-02 13:49:56 +0200

    fix top dl

--- cogs/cardsearch.py
+++ cogs/cardsearch.py
@@ -50,7 +50,7 @@ class CardSearch(commands.Cog):
     @commands.command(name="top")
     async def topDL(self, ctx):
         """List of top deck list"""
-        await ctx.send("ALL => " + URL_TOP_DL)
+        await ctx.send("ALL => " + config.URL_TOP_DL)
         #await ctx.send("YCS => " + config.URL_TOP_YCS)
         #await ctx.send("REGIO => " + config.URL_TOP_REGIO)
 
