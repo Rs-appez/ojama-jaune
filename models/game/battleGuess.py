164afcd216f5c0689d6d11eb58e2f775518ae156
Rs-appez <alexandrep1999@hotmail.com>
2023-08-09 16:03:09 +0200

    add start/get_cards method WIP

--- models/game/battleGuess.py
+++ models/game/battleGuess.py
@@ -1,5 +1,8 @@
+from models.card.cards import Cards
 from views.game.battle.registerView import RegisterView
 import asyncio
+
+from views.game.battle.starterView import StarterView
 
 class BattleGuess():
 
@@ -8,8 +11,21 @@ class BattleGuess():
         self.emojis = emojis
         self.players = []
         self.author = author
+        self.cards = []
 
     async def setup(self):
         msg = await self.channel.send('Player : \npersonne 😭')
         await self.channel.send('Join the party !',view=RegisterView(self.players,self.emojis["aqua"],msg))
-        await self.author.send('test')
+        self.get_cards()
+        await self.author.send('Demarrer la partie pour tout les joueurs.',view=StarterView(self))
+        print(self.cards)
+
+    async def start(self):
+        print('ok')
+
+    def get_cards(self):
+        
+        for i in range(5):
+            self.cards.append(Cards.get_random_card())
+
+        
