dcf664892138b75e58565af19bbc68ed079f6edd
Rs-appez <alexandrep1999@hotmail.com>
2023-09-08 14:42:44 +0200

    ça va être chiant ...

--- quickstart.py
+++ quickstart.py
@@ -2,25 +2,46 @@ import requests
 import json
 import config
 
+
+def get_json_sheet(id_sheet,include_grid_data=True):
+    url_request = f"{config.URL_SHEET_API}{id_sheet}?includeGridData={include_grid_data}&key={config.GOOGLE_API_KEY}"
+    res = requests.get(url_request)
+    return res.json()
+
+def update_json_sheet(id_sheet,include_grid_data=True):
+    res_json = get_json_sheet(id_sheet,include_grid_data)
+    with open('test.json','w') as fp:
+        json.dump(res_json, fp)
+
+def read_json_sheet(json_name):
+    with open(json_name) as json_file:
+        data = json.load(json_file)
+    return data
+
 def main():
     id_sheet = "12dhcPy3Z4DoVNZHV7EJOQN4DlwaSxF0aK1Nim4BJSgo"
-    include_grid_data = False
-    url_request = f"{config.URL_SHEET_API}{id_sheet}?includeGridData={include_grid_data}&key={config.GOOGLE_API_KEY}"
+    include_grid_data = True
     sheets_name = []
 
-    res = requests.get(url_request)
+    # update_json_sheet(id_sheet,include_grid_data)
 
-    # res_json = res.json()
+    res_json = read_json_sheet('test.json')
 
-    print(res)
-    # for sheet in res_json["sheets"] :
-    #     sheets_name.append(sheet['properties']['title'])
+    for sheet in res_json["sheets"] :
+        sheets_name.append(sheet['properties']['title'])
+        for d in sheet['data']:
+            for r in d['rowData']:
+                print(r['values'])
+                break
+                for s in r['values']:
+                    if 'effectiveValue' in s:
+                        if 'stringValue' in s['effectiveValue']:
+                            print(s['effectiveValue']['stringValue'])
 
-    # print(sheets_name)
+                print('------------------')
 
-    with open('test.json','w') as fp:
+    print(sheets_name)
 
-        json.dump(res.json(), fp)
 
 if __name__ == '__main__':
     main()
