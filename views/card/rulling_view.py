9c3d539f8311b9ac57c9e8cafe2c4813714332ac
Rs-appez <37080870+Rs-appez@users.noreply.github.com>
2025-04-10 04:33:30 +0200

    remove unused buggy import

--- views/card/rulling_view.py
+++ views/card/rulling_view.py
@@ -1,24 +1,19 @@
-from re import T
 import nextcord
-from nextcord.ext import commands
 from nextcord.interactions import Interaction
 
 from models.card.cards import Cards
 
 
 class RullingView(nextcord.ui.View):
-
-    def __init__(self, card : Cards):
+    def __init__(self, card: Cards):
         self.card = card
         super().__init__()
 
     @nextcord.ui.button(label="Voir rulling", style=nextcord.ButtonStyle.primary)
-    async def view_rulling(self, button, interaction : Interaction):
+    async def view_rulling(self, button, interaction: Interaction):
         button.disabled = True
         await interaction.response.defer(with_message=True)
-        await interaction.followup.edit_message(message_id = interaction.message.id, view = self)
+        await interaction.followup.edit_message(
+            message_id=interaction.message.id, view=self
+        )
         await self.card.rulling(interaction)
-        
-        
-
-
